b354120c30902555dae2c186f8844a74e3695fa5
brigitapop@gmail.com <brigitapop@gmail.com>
2019-07-20 09:45:12 +0300

    initial commit

---
+++ UnitTesting-InternshipSummer2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTesting_InternshipSummer2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
